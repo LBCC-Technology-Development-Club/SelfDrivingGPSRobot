10912ee7ee77c7036a42cc15570f53012aed9725
Patrick Suhm <45735202+PatrickSuhm@users.noreply.github.com>
2019-10-31 19:37:19 +0100

    Add files via upload

---
+++ gps.h
@@ -0,0 +1,140 @@
+#ifndef GPS_H
+#define GPS_H
+
+#include "constants.h"
+
+//@note: The code in this file is based on the work of iforce2d: https://www.youtube.com/watch?v=TwhCX0c8Xe0
+
+namespace gps
+{
+  struct geoLocFloat {
+    float lat;
+    float lon;
+  };
+
+const char UBLOX_INIT[] PROGMEM = {
+  //switch all message types off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x24, // GxGGA off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x01,0x00,0x00,0x00,0x00,0x00,0x01,0x01,0x2B, // GxGLL off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x02,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x32, // GxGSA off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x03,0x00,0x00,0x00,0x00,0x00,0x01,0x03,0x39, // GxGSV off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x04,0x00,0x00,0x00,0x00,0x00,0x01,0x04,0x40, // GxRMC off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0xF0,0x05,0x00,0x00,0x00,0x00,0x00,0x01,0x05,0x47, // GxVTG off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x12,0xB9, //NAV-POSLLH off
+  0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x13,0xC0, //NAV-STATUS off
+
+  //then switch on the desired one
+  0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x02,0x00,0x01,0x00,0x00,0x00,0x00,0x13,0xBE, //NAV-POSLLH on
+  //0xB5,0x62,0x06,0x01,0x08,0x00,0x01,0x03,0x00,0x01,0x00,0x00,0x00,0x00,0x14,0xC5, //NAV-STATUS on
+
+  //choose frequency
+  0xB5,0x62,0x06,0x08,0x06,0x00,0x64,0x00,0x01,0x00,0x01,0x00,0x7A,0x12, //(10Hz)
+  //0xB5,0x62,0x06,0x08,0x06,0x00,0xC8,0x00,0x01,0x00,0x01,0x00,0xDE,0x6A, //(5Hz)
+  //0xB5,0x62,0x06,0x08,0x06,0x00,0xE8,0x03,0x01,0x00,0x01,0x00,0x01,0x39, //(1Hz)
+};
+
+const unsigned char UBX_HEADER[] = { 0xB5, 0x62 };
+
+struct NAV_POSLLH {
+  unsigned char cls;
+  unsigned char id;
+  unsigned short len;
+  unsigned long iTOW;
+  long lon;
+  long lat;
+  long height;
+  long hMSL;
+  unsigned long hAcc;
+  unsigned long vAcc;
+};
+
+NAV_POSLLH gpsData;
+
+void setupGPS()
+{
+  //@brief: send configuration data in UBX protocol
+  for(int i = 0; i < sizeof(UBLOX_INIT); i++) {                        
+    Serial2.write( pgm_read_byte(UBLOX_INIT+i) );
+    delay(5); // simulating a 38400baud pace (or less), otherwise commands are not accepted by the device.
+  }
+  Serial1.println("GPS configured.");
+}
+
+void calcChecksum(unsigned char* CK) {
+  //@brief: calculate the checksum over the posllh data
+  memset(CK, 0, 2);
+  for (int i = 0; i < (int)sizeof(NAV_POSLLH); i++) {
+    CK[0] += ((unsigned char*)(&gpsData))[i];
+    CK[1] += CK[0];
+  }
+}
+
+bool processGPS() {
+  //@brief: reads the posllh gps data via serial2 from ublox module if its available. 
+  static int fpos = 0;
+  static unsigned char checksum[2];
+  const int payloadSize = sizeof(NAV_POSLLH);
+
+  while ( Serial2.available() ) {
+    byte c = Serial2.read();
+    if ( fpos < 2 ) {
+      if ( c == UBX_HEADER[fpos] )
+        fpos++;
+      else
+        fpos = 0;
+    }
+    else {
+      if ( (fpos-2) < payloadSize )
+        ((unsigned char*)(&gpsData))[fpos-2] = c;
+
+      fpos++;
+
+      if ( fpos == (payloadSize+2) ) {
+        calcChecksum(checksum);
+      }
+      else if ( fpos == (payloadSize+3) ) {
+        if ( c != checksum[0] )
+          fpos = 0;
+      }
+      else if ( fpos == (payloadSize+4) ) {
+        fpos = 0;
+        if ( c == checksum[1] ) {
+          return true;
+        }
+      }
+      else if ( fpos > (payloadSize+4) ) {
+        fpos = 0;
+      }
+    }
+  }
+  return false;
+}
+
+
+float getDistance(geoLocFloat a, geoLocFloat b) {
+  //@brief: returns position between a and b in meters
+  const float R = 6371000; //earth radius in meter
+  float p1 = a.lat * toRad;
+  float p2 = b.lat * toRad;
+  float dp = (b.lat-a.lat) * toRad;
+  float dl = (b.lon-a.lon) * toRad;
+
+  float x = sin(dp/2) * sin(dp/2) + cos(p1) * cos(p2) * sin(dl/2) * sin(dl/2);
+  float y = 2 * atan2(sqrt(x), sqrt(1-x));
+
+  return R * y;
+}
+
+float getBearing(geoLocFloat a, geoLocFloat b) {
+  //@brief: returns the angle between (a,b) and (a,North) in Degree.
+  float aLat = a.lat*toRad; 
+  float aLon = a.lon*toRad;
+  float bLat = b.lat*toRad; 
+  float bLon = b.lon*toRad;
+  float y = sin(bLon-aLon) * cos(bLat);
+  float x = cos(aLat)*sin(bLat) - sin(aLat)*cos(bLat)*cos(bLon-aLon);
+  return atan2(y, x) * toDeg;
+}
+
+}
+#endif
