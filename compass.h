560f9a969dcc1372c41b85d4c4bfe99da467008a
Patrick Suhm <45735202+PatrickSuhm@users.noreply.github.com>
2019-10-31 19:31:32 +0100

    Update compass.h

--- compass.h
+++ compass.h
@@ -6,22 +6,23 @@
 
 namespace compass
 {
-  HMC5883L_Simple Compass;
+  	HMC5883L_Simple Compass;
 
-void setupCompass()
-{
-	Wire.begin();
-	Compass.SetDeclination(2, 31, 'E');  // http://www.magnetic-declination.com/
-	Compass.SetSamplingMode(COMPASS_CONTINUOUS); //COMPASS_CONTINUOUS COMPASS_SINGLE
-	Compass.SetScale(COMPASS_SCALE_130); //Sensitivity
-	Compass.SetOrientation(COMPASS_HORIZONTAL_Y_NORTH); //this depends on how its mounted on the robot
-	Serial1.println("Compass initialized.");
-}
+	void setupCompass()
+	{
+		Wire.begin();
+		Compass.SetDeclination(2, 31, 'E');  // http://www.magnetic-declination.com/
+		Compass.SetSamplingMode(COMPASS_CONTINUOUS); //COMPASS_CONTINUOUS COMPASS_SINGLE
+		Compass.SetScale(COMPASS_SCALE_130); //Sensitivity
+		Compass.SetOrientation(COMPASS_HORIZONTAL_Y_NORTH); //this depends on how its mounted on the robot
+		Serial1.println("Compass initialized.");
+	}
 
-float getHeading()
-{//@brief: returns the current heading and substracts 180 Deg because the compass is mounted with zero pointing to the back
-	 return Compass.GetHeadingDegrees()-180.0;
-}
+	float getHeading()
+	{
+		//@brief: returns the current heading and substracts 180 Deg because the compass is mounted with zero pointing to the back
+		return Compass.GetHeadingDegrees()-180.0;
+	}
 
 }
 
